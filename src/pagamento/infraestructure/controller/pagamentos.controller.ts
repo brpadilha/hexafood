6113ae4a3163e3b57328804d6559bba3dc0bd30b
Bruno <bruno.rangel@ammovarejo.com.br>
2023-08-31 22:40:19 -0300

    feat(pagamento): fix pagamento controller

--- src/pagamento/infraestructure/controller/pagamentos.controller.ts
+++ src/pagamento/infraestructure/controller/pagamentos.controller.ts
@@ -1,36 +1,26 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CreatePagamentoDto } from '../../core/application/usecases/pagamento/pagamentoDto';
+import { CreatePagamentoUseCase } from 'src/pagamento/core/application/usecases/pagamento/create.pagamento.usecase';
+import { FindPagamentoUseCase } from 'src/pagamento/core/application/usecases/pagamento/find.pagamento.usecase';
 
 @ApiTags('pagamentos')
 @Controller('pagamentos')
 export class PagamentosController {
-  constructor() {}
+  constructor(
+    private readonly createPagamentoUseCase: CreatePagamentoUseCase,
+    private readonly findPagamentoUseCase: FindPagamentoUseCase
 
-
-  // export class ClientesController {
-  //   constructor(
-  //     private readonly createClienteUseCase: CreateClienteUseCase,
-  //     private readonly identiyClienteUseCase: IndentifyClienteUseCase,
-  //   ) {}
+  ) {}
   
-  // @Post()
-  // create(@Body() createPagamentoDto: CreatePagamentoDto) {
-  //   return this.pagamentosService.createPagamento(createPagamentoDto);
-  // }
+  @Post()
+  create(@Body() createPagamentoDto: CreatePagamentoDto) {
+    return this.createPagamentoUseCase.execute(createPagamentoDto);
+  }
 
-  // @Get()
-  // findAll() {
-  //   return this.pagamentosService.findAll();
-  // }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.findPagamentoUseCase.execute(+id);
+  }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.pagamentosService.findById(+id);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.pagamentosService.remove(+id);
-  // }
 }
